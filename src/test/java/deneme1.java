3fbb2b2819b1b1846a53d6e8c0651a618d0af4cf
MikeJ77 <mikejohnson3505@gmail.com>
2022-01-06 22:05:58 +0300

    second commit

--- src/test/java/deneme1.java
+++ src/test/java/deneme1.java
@@ -1,2 +1,7 @@
-package PACKAGE_NAME;public class deneme1 {
+public class deneme1 {
+
+    public static void main(String[] args) {
+        System.out.println("try1");
+        //commit 2
+    }
 }
